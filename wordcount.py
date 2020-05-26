e4c2586e576c83bf6c5a4234313ac54eea3e51ad
Albina1993 <alba.tleubergen@gmail.com>
2020-05-26 01:43:27 -0400

    commiting wordcount

--- wordcount.py
+++ wordcount.py
@@ -1,57 +1,39 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-"""Wordcount exercise
-
-The main() function is already defined and complete. It calls the
-print_words() and print_top() functions, which you fill in.
-
-See the README for instructions.
-
-Use str.split() (no arguments) to split on all whitespace.
-
-Workflow: don't build the whole program at once. Get it to an intermediate
-milestone and print your data structure. Once that's working, try for the
-next milestone.
-
-Implement the create_word_dict() helper function that has been defined in
-order to avoid code duplication within print_words() and print_top(). It
-should return a dictionary with words as keys, and their counts as values.
-"""
-
-# Your name, plus anyone who helped you with this assignment
-# Give credit where credit is due.
-__author__ = "???"
+__author__ = "Albina Tileubergen-Thomas help from Chris Warren, Cheria Artis"
 
 import sys
+import re
 
 
 def create_word_dict(filename):
-    """Returns a word/count dict for the given file."""
-    # Your code here
-    return
+    word_count = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            words = line.split()
+            for word in words:
+                each_word = word.lower()
+                if each_word in word_count:
+                    word_count[each_word] += 1
+                else:
+                    word_count[each_word] = 1
+    return word_count
 
 
 def print_words(filename):
-    """Prints one per line '<word> : <count>', sorted
-    by word for the given file.
-    """
-    # Your code here
-    return
+    word_dict = create_word_dict(filename)
+    for item in sorted(word_dict):
+        print(item, ":", word_dict[item])
+    return word_dict
 
 
 def print_top(filename):
-    """Prints the top count listing for the given file."""
-    # Your code here
-    return
+    word_dict = create_word_dict(filename)
+    top_twenty = sorted(word_dict, key=lambda item: -word_dict[item])[:20]
+    for item in top_twenty:
+        print(item + " : " + str(word_dict[item]))
+    return word_dict
 
 
-# This basic command line argument parsing code is provided and calls
-# the print_words() and print_top() functions which you must implement.
 def main(args):
     if len(args) != 2:
         print('usage: python wordcount.py {--count | --topcount} file')
